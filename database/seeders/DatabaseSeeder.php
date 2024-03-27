54c6cde02acb90025f7928b7e56be0eaa234548d
jdfs2110 <jdfsiescamp@gmail.com>
2024-03-27 17:11:09 +0100

    created base roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Roles;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        /* User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]); */
+
+        $roles = ['mesero', 'cocina', 'rrhh', 'admin'];
+
+        foreach($roles as $role) {
+            Roles::factory()->create([
+                'nombre' => $role
+            ]);
+        }
     }
 }
