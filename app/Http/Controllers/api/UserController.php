b0a767bf424a014d2d2ae9799ed03edb06852455
jdfs2110 <jdfsiescamp@gmail.com>
2024-03-27 16:44:30 +0100

    UserController (empty)

---
+++ app/Http/Controllers/api/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+}
