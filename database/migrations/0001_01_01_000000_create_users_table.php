7e35383ccc49f7ca5393568644afac92d127888b
jdfs2110 <jdfsiescamp@gmail.com>
2024-03-27 16:42:15 +0100

    i forgor softdelete

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->date('fecha_ingreso');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
