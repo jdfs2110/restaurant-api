9df7697dcbf6bc0b68a855ffedcc72cd7e0d5847
jdfs2110 <jdfsiescamp@gmail.com>
2024-03-27 15:28:00 +0100

    added sanctum for tokens

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::group(['middleware' => ['auth-sanctum']], function() {
+   //protected endpoints
+});
