9fb37fcefb3440efa3c69a0d87be6496e20b7063
jdfs2110 <jdfsiescamp@gmail.com>
2024-03-28 15:19:29 +0100

    Resource para devolver el nombre de rol del usuario

---
+++ app/Http/Resources/UsuarioResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UsuarioResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return[
+            'id' => $this->getId(),
+            'name' => $this->getName(),
+            'email' => $this->getEmail(),
+            'estado' => $this->getEstado(),
+            'fecha_ingreso' => $this->getFechaIngreso(),
+            'id_rol' => $this->getIdRol(),
+            'rol' => $this->getRol(),
+            'created_at' => $this->getCreatedAt(),
+            'updated_at' => $this->getUpdatedAt(),
+            'deleted_at' => $this->getDeletedAt()
+        ];
+    }
+}
