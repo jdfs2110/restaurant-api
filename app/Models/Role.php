561a33211802bdaf1015aeaba256df7057959913
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-02 17:16:52 +0200

    added more getters

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -19,8 +19,28 @@ class Role extends Model
         'nombre',
     ];
 
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
     public function getNombre(): string
     {
         return $this->nombre;
+    }
+
+    public function getCreatedAt(): string | null
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt(): string | null
+    {
+        return $this->updated_at;
+    }
+
+    public function getDeletedAt(): string | null
+    {
+        return $this->deleted_at;
     }
 }
