acb1929873e18af3980c8d29d93b389cec742f53
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-02 17:39:56 +0200

    updatePedido function + fix

--- app/Http/Controllers/api/PedidoController.php
+++ app/Http/Controllers/api/PedidoController.php
@@ -43,10 +43,10 @@ class PedidoController extends Controller
     function newPedido(Request $request): JsonResponse
     {
         $pedidoData = $request->validate([
-            'precio' => 'numeric|required',
-            'numero_comensales' => 'int|required|min:1',
-            'id_mesa' => 'int|required',
-            'id_usuario' => 'int|required'
+            'precio' => 'required|numeric',
+            'numero_comensales' => 'required|int|min:1',
+            'id_mesa' => 'required|int',
+            'id_usuario' => 'required|int'
         ]);
 
         $pedido = Pedido::query()->create([
@@ -59,6 +59,45 @@ class PedidoController extends Controller
         ]);
 
         $response = new PedidoResource($pedido);
+
+        return response()->json($response);
+    }
+
+    function updatePedido(Request $request, $id): JsonResponse
+    {
+        $pedidoData = $request->validate([
+            'estado' => 'required|int|max:3',
+            'precio' => 'required|numeric',
+            'numero_comensales' => 'required|int|min:1',
+            'id_mesa' => 'required|int',
+            'id_usuario' => 'required|int'
+        ]);
+
+        $pedido = Pedido::query()->where('id', $id)->get()->first();
+
+        if (is_null($pedido)) {
+            $errorMessage = [
+                'error' => 'El pedido no existe.'
+            ];
+
+            return response()->json($errorMessage, 404);
+        }
+
+        $update = Pedido::query()->where('id', $id)->update([
+            'estado' => $pedidoData['estado'],
+            'precio' => $pedidoData['precio'],
+            'numero_comensales' => $pedidoData['numero_comensales'],
+            'id_mesa' => $pedidoData['id_mesa'],
+            'id_usuario' => $pedidoData['id_usuario']
+        ]);
+        $message = $update == 1 ? 'El pedido ha sido modificado correctamente.' : 'Error al modificar el pedido';
+
+        $updatedPedido = Pedido::query()->where('id', $id)->get()->first();
+
+        $response = [
+            'pedido' => new PedidoResource($updatedPedido),
+            'message' => $message
+        ];
 
         return response()->json($response);
     }
