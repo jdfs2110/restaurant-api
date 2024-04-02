d7dae3b02c865191fd684f953cc47a1e1fa59ec7
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-02 16:38:20 +0200

    getAllUsersByRole

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PedidoResource;
 use App\Http\Resources\UsuarioResource;
+use App\Models\Role;
 use App\Models\User;
 use App\Models\Pedido;
 use Illuminate\Http\JsonResponse;
@@ -43,6 +44,35 @@ class UserController extends Controller
         $pedidos = Pedido::query()->where('id_usuario', $id)->get();
 
         $response = PedidoResource::collection($pedidos);
+
+        return response()->json($response);
+    }
+
+    public function getAllUsersByRole($id): JsonResponse
+    {
+        $role = Role::query()->where('id', $id)->get()->first();
+
+        if (is_null($role)) {
+            $errorMessage = [
+                'error' => 'El rol no existe.'
+            ];
+
+            return response()->json($errorMessage, 404);
+        }
+
+        $users = User::query()->where('id_rol', $id)->get();
+
+        if (is_null($users)) {
+            $errorMessage = [
+                'error' => 'No hay usuarios con este rol.'
+            ];
+
+            return response()->json($errorMessage, 404);
+        }
+
+        $response = [
+            'usuarios' => $users
+        ];
 
         return response()->json($response);
     }
