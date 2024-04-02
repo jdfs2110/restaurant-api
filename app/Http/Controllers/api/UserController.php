669c74ed22ff1cccde1189b36f693ebf669450b2
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-02 16:52:10 +0200

    updateUser function

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -9,6 +9,8 @@ use App\Models\Role;
 use App\Models\User;
 use App\Models\Pedido;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
 class UserController extends Controller
 {
     public function index(): JsonResponse
@@ -72,6 +74,43 @@ class UserController extends Controller
 
         $response = [
             'usuarios' => $users
+        ];
+
+        return response()->json($response);
+    }
+
+    public function updateUser(Request $request, $id): JsonResponse
+    {
+        $userData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+            'id_rol' =>  'required|int'
+        ]);
+
+        $user = User::query()->where('id', $id)->get()->first();
+
+        if (is_null($user)) {
+            $errorMessage = [
+                'error' => 'El usuario no existe.'
+            ];
+
+            return response()->json($errorMessage, 404);
+        }
+
+        $update = User::query()->where('id', $id)->update([
+            'name' => $userData['name'],
+            'email' => $userData['email'],
+            'password' => bcrypt($userData['password']),
+            'id_rol' => $userData['id_rol']
+        ]);
+        $message = $update == 1 ? 'El usuario ha sido modificado correctamente.' : 'Error al modificar el usuario';
+
+        $updatedUser = User::query()->where('id', $id)->get()->first();
+
+        $response = [
+            'usuario' => new UsuarioResource($updatedUser),
+            'message' => $message
         ];
 
         return response()->json($response);
