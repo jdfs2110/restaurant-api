3312dac04022e8c1ad9cddc7b5446eb187b95f53
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-05 09:43:53 +0200

    added tests (i have to setup the testing db (:sleeping:)

---
+++ tests/Feature/RoleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RoleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_get_all_roles(): void
+    {
+        $response = $this->get('/api/roles');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_get_role_by_id(): void
+    {
+        $response = $this->get('/api/roles/1');
+
+        $response->assertStatus(200);
+    }
+}
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_incorrect_login(): void
+    {
+        $response = $this->withHeaders(['Accept' => 'application/json'])->post('/api/login', ['email' => 'test@test.com', 'password' => '12345']);
+
+        $response
+            ->assertStatus(400)
+            ->assertJson(['error' => 'Usuario o contraseña incorrectos.']);
+    }
+
+    public function test_successful_login(): void
+    {
+        $response = $this->withHeaders(['Accept' => 'application/json'])->post('/api/login', ['email' => 'test@test.com', 'password' => '123456']);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+            $json->hasAll(['data', 'token'])
+            );
+    }
+
+    public function test_incorrect_register(): void
+    {
+        $data = [
+            'name' => 'already existing user',
+            'email' => 'test@test.com',
+            'password' => '123456',
+            'password_confirmation' => '123456',
+            'id_rol' => 1
+        ];
+        $response = $this->withHeaders(['Accept' => 'application/json'])->post('/api/registro', $data);
+
+        $response
+            ->assertStatus(422)
+            ->assertJson(fn (AssertableJson $json) =>
+            $json->hasAny('errors', 'message')
+            );
+
+    }
+
+    public function test_successful_register(): void
+    {
+        $data = [
+            'name' => 'new, non-existing user',
+            'email' => 'laravel@test.php',
+            'password' => '123456',
+            'password_confirmation' => '123456',
+            'id_rol' => 1
+        ];
+        $response = $this->withHeaders(['Accept' => 'application/json'])->post('/api/registro', $data);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+            $json->hasAny('data', 'token')
+            );
+    }
+}
