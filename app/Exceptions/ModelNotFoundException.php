78f81de74937de8e878631d0a6277e721872efd7
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-11 12:34:59 +0200

    added exceptions (i believe they have no use right now)

---
+++ app/Exceptions/ModelNotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Exceptions;
+
+class ModelNotFoundException extends \Exception
+{
+}
---
+++ app/Exceptions/NegativeQuantityException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Exceptions;
+
+class NegativeQuantityException extends \Exception
+{
+
+}
