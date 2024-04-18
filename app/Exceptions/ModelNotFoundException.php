05d2b97fdbae75d614857989163153be5208f233
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-18 16:12:15 +0200

    gave ussage to modelnotfoundexception

--- app/Exceptions/ModelNotFoundException.php
+++ app/Exceptions/ModelNotFoundException.php
@@ -2,6 +2,13 @@
 
 namespace App\Exceptions;
 
-class ModelNotFoundException extends \Exception
+use Exception;
+use Throwable;
+
+class ModelNotFoundException extends Exception
 {
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
