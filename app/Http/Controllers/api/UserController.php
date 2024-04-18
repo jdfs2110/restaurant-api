cae544066f8a75880e4b8fa5381e9aa7fc5a9441
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-18 16:11:45 +0200

    update usercontroller to use validation exception & modelnotfound exception

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Exceptions\ModelNotFoundException;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PedidoResource;
 use App\Http\Resources\UsuarioResource;
-use App\Models\Role;
-use App\Models\User;
-use App\Models\Pedido;
 use App\Repositories\PedidoRepository;
 use App\Repositories\RoleRepository;
 use App\Repositories\UserRepository;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -38,8 +37,10 @@ class UserController extends Controller
             $user = $this->repository->findOrFail($id);
 
             return $this->successResponse(new UsuarioResource($user));
-        } catch (Exception $e) {
+        } catch (ModelNotFoundException $e) {
             return $this->errorResponse($e->getMessage());
+        } catch (Exception $e) {
+            return $this->errorResponse($e->getMessage(), 400);
         }
     }
 
@@ -47,49 +48,60 @@ class UserController extends Controller
     {
         try {
             $this->repository->findOrFail($id);
+
             $pedidos = $this->pedidoRepository->findPedidosByIdUsuario($id);
-        } catch (Exception $e) {
+
+            return $this->successResponse(PedidoResource::collection($pedidos));
+        } catch (ModelNotFoundException $e) {
             return $this->errorResponse($e->getMessage());
+        } catch (Exception $e) {
+            return $this->errorResponse($e->getMessage(), 400);
         }
 
-        return $this->successResponse(PedidoResource::collection($pedidos));
     }
 
     public function getAllUsersByRole($id): JsonResponse
     {
         try {
             $this->roleRepository->findOrFail($id);
-            $users = $this->repository->findAllByIdRol($id);
-        } catch (Exception $e) {
-            return $this->errorResponse($e->getMessage());
-        }
 
-        return $this->successResponse(UsuarioResource::collection($users));
+            $users = $this->repository->findAllByIdRol($id);
+
+            return $this->successResponse(UsuarioResource::collection($users));
+        } catch (ModelNotFoundException $e) {
+            return $this->errorResponse($e->getMessage());
+        } catch (Exception $e) {
+            return $this->errorResponse($e->getMessage(), 400);
+        }
     }
 
     public function updateUser(Request $request, $id): JsonResponse
     {
-        $data = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|unique:users,email',
-            'password' => 'required|string|confirmed',
-            'id_rol' => 'required|int'
-        ]);
-
         try {
+            $data = $request->validate([
+                'name' => 'required|string',
+                'email' => 'required|string|unique:users,email',
+                'password' => 'required|string|confirmed',
+                'id_rol' => 'required|int'
+            ]);
+
             $user = $this->repository->findOrFail($id);
-        } catch (Exception $e) {
+
+            $update = $user->update([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => bcrypt($data['password']),
+                'id_rol' => $data['id_rol']
+            ]);
+            $message = $update == 1 ? 'El usuario ha sido modificado correctamente.' : 'Error al modificar el usuario';
+
+            return $this->successResponse(new UsuarioResource($user), $message);
+        } catch (ValidationException $e) {
+            return $this->errorResponse($e->errors(), 400);
+        } catch (ModelNotFoundException $e) {
             return $this->errorResponse($e->getMessage());
+        } catch (Exception $e) {
+            return $this->errorResponse($e->getMessage(), 400);
         }
-
-        $update = $user->update([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-            'id_rol' => $data['id_rol']
-        ]);
-        $message = $update == 1 ? 'El usuario ha sido modificado correctamente.' : 'Error al modificar el usuario';
-
-        return $this->successResponse(new UsuarioResource($user), $message);
     }
 }
