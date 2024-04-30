7727d2c8e3cea48b28e65a74a06d78e50eadc019
jdfs2110 <jdfsiescamp@gmail.com>
2024-04-30 14:47:43 +0200

    la mama de la mama¿

--- app/Exceptions/EmailAlreadyInUseException.php
+++ app/Exceptions/EmailAlreadyInUseException.php
@@ -5,6 +5,9 @@ namespace App\Exceptions;
 use Exception;
 use Throwable;
 
+/**
+ * Excepción que se lanza cuando se intenta poner un email que ya está en uso al editar un usuario
+ */
 class EmailAlreadyInUseException extends Exception
 {
     public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null)
