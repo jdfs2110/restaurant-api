4c9b51fa6fe967172306f0d3ba9feb6e070a0764
jdfs2110 <jdfsiescamp@gmail.com>
2024-05-03 09:50:15 +0200

    refactor(routes): quitado todo menos api y healthcheck
    Co-authored-by: Juan Daniel Fornai Barriga

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,10 +6,10 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+//        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
+//        commands: __DIR__.'/../routes/console.php',
+//        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
