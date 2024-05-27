76e7f22ce43cad66955737f4164cd8608f2921e4
jdfs2110 <jdfsiescamp@gmail.com>
2024-05-27 16:53:22 +0200

    xdd

--- app/Repositories/ProductoRepository.php
+++ app/Repositories/ProductoRepository.php
@@ -31,7 +31,8 @@ class ProductoRepository extends GeneralRepository
             ->join('categorias', 'categorias.id', '=', 'productos.id_categoria')
             ->join('stock', 'productos.id', '=', 'stock.id_producto')
             ->where('productos.id', $id)
-            ->get()->first();
+            ->where('productos.deleted_at', '=', null)
+            ->get()->firstOrFail();
 
         $producto->foto = env('CLOUDFLARE_R2_URL') . '/' . $producto->foto;
 
